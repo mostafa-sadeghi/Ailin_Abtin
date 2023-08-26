22ec3c10bdb16b7875a11b3faf2bdb90b2b7d69d
msa <msai.sadeghi@gmail.com>
2023-08-26 17:27:40 +0330

    s

---
+++ session1.py
@@ -0,0 +1,43 @@
+# print(1 + 1)
+# print(1 - 1)
+# print(1 * 7)
+# print(1 / 7)
+#
+# print(5 / 2)
+# print(5 // 2)
+# print(5 % 2)
+#
+#
+# x = 12
+# print("x =", x)
+# print(type(x))
+
+# y = 1.5
+# print(type(y))
+
+
+# print('ailin')
+
+name = "abtin"
+# print(type(name))
+print(name[0])
+print(name[1])
+print("length:", len(name))
+
+name = input("enter a name: ")
+# print("hello", name)
+message = "hello " + name
+print(message)
+
+"""
+برنامه ای بنویسید که اسم کاربر را از ورودی دریاف نماید و کارکترهای آن را با فاصله از هم نمایش دهد
+sara
+s a r a
+
+abtin
+a b t i n
+
+ailin
+a i l i n
+
+"""
---
+++ session2.py
@@ -0,0 +1,25 @@
+# x = '12'
+# print(x[0])
+# print(x[1])
+
+# name = "nikan"
+# name = 'nikan'
+
+# message = "hello everybody. \nwelcome to python class. \nwe are going to learn everythings about python"
+# print(message)
+# message = """hello everybody. 
+# welcome to python class. 
+# we are going to learn everythings about python"""
+# print(message)
+
+
+# message = "He said: \"you must go some where\""
+# print(message)
+# message = 'He said: "you must go some where"'
+# print(message)
+
+
+user_name = input("Enter the userName: ")
+password = input("Enter the password: ")
+
+print("userName is:", user_name, "and password is:", password)
