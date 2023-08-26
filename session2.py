8e0c4c166635a5746f26e3514553d65befe02a6a
msa <msai.sadeghi@gmail.com>
2023-08-26 17:58:24 +0330

    s

--- session2.py
+++ session2.py
@@ -7,8 +7,8 @@
 
 # message = "hello everybody. \nwelcome to python class. \nwe are going to learn everythings about python"
 # print(message)
-# message = """hello everybody. 
-# welcome to python class. 
+# message = """hello everybody.
+# welcome to python class.
 # we are going to learn everythings about python"""
 # print(message)
 
@@ -19,7 +19,35 @@
 # print(message)
 
 
-user_name = input("Enter the userName: ")
-password = input("Enter the password: ")
+# user_name = input("Enter the userName: ")
+# password = input("Enter the password: ")
 
-print("userName is:", user_name, "and password is:", password)
+# print("userName is:", user_name, "and password is:", password)
+# print("userName is: " + user_name + " and password is: " + password)
+# print(f"userName is: {user_name} and password is: {password}")
+
+
+# name = input("Enter your name: ")
+# family = input("Enter your family: ")
+# message = f"your name is: {name} and your family is: {family}"
+# print(message)
+
+# TODO برنامه ای بنویسید که نام، نام خانوادگی و سن فردی را بگیرد
+# و متنی مشابه زیر نمایش دهد
+# arsham rezaei has 18 years old.
+
+
+# x = float(input("enter a number: "))
+# y = float(input("enter a number: "))
+
+# z = x + y 
+# print(z)
+
+number_1 = int(input("Enter first Number: "))
+number_2 = int(input("Enter second Number: "))
+number_3 = int(input("Enter third Number: "))
+
+print(f"{number_1} + {number_2} + {number_3} = {number_1 + number_2 + number_3}")
+print(f"{number_1} + {number_2} - {number_3} = {number_1 + number_2 - number_3}")
+print(f"{number_1} * {number_2} + {number_3} = {number_1 * number_2 + number_3}")
+print(f"{number_1} / {number_2} + {number_3} = {number_1 / number_2 + number_3}")
