bb5f3bfa37f38a94cbe09618507dcdebc2d03e8e
msa <msai.sadeghi@gmail.com>
2023-09-02 17:58:01 +0330

    s

---
+++ session3.py
@@ -0,0 +1,79 @@
+# names = ["abtin", "ailin"]
+
+# print(type(names))
+# print(len(names))
+
+# print(names[0])
+# print(names[1])
+
+# names.append("rozha")
+# print(names[0], names[1], names[2])
+
+# names[2] = "sara"
+# print(names)
+
+
+# names.remove(names[2])
+
+# print(names)
+
+# names.pop()
+# print(names)
+
+# del names[0]
+# print(names)
+
+# my_list = [1, 2, 3, ["nima", "nikan", ["roze","sara"]]]
+# print(my_list[3][0])
+# print(my_list[3][1])
+# print(my_list[3][2][0])
+# print(my_list[3][2][1])
+
+# names = ("ailin", "abtin")
+# print(type(names))
+# print(len(names))
+# print(names[0])
+# print(names[1])
+
+# names[1] = "sara" # error
+
+# favorite_colors = {
+#     'abtin': 'green',
+#     'ailin': 'purple'
+# }
+
+
+# print(f"ailin Likes {favorite_colors['ailin']}")
+# print(f"abtin Likes {favorite_colors['abtin']}")
+
+# favorite_colors['roze'] = "red"
+# print(favorite_colors)
+
+# del favorite_colors['roze']
+# print(favorite_colors)
+
+
+# TODO
+"""
+برنامه ای بنویسید که نام سه نفر را از ورودی دریافت نماید
+و داخل لیستی اضافه نماید
+
+برنامه ای بنویسید که سه عدد از ورودی دریافت نماید و داخل  لیستی اضافه کند و سپس اعداد موجود
+در لیست را با هم جمع نماید
+
+برنامه ای بنویسید که نام و غذای مورد علاقه سه نفر را از ورودی دریافت نماید و داخل یک دیکشنری ذخیره کند
+
+"""
+
+
+a = 13
+b = 13
+
+
+if a > b:
+    print("a is greater than b")
+elif a == b:
+    print("a and b are equal")
+
+else:
+    print("a is less than b")
