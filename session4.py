4e506b63c11d6ba5ee02639686e3c1c626c3f74f
msa <msai.sadeghi@gmail.com>
2023-09-09 17:59:04 +0330

    s

---
+++ session4.py
@@ -0,0 +1,26 @@
+# for i in range(5):
+#     print("hello", i)
+
+# for x in range(1,10):
+#     print('hello', x)
+
+# for x in range(1,10,2): # 1 3 5 7 9
+#     print('hello', x)
+
+
+# for x in range(2,10,2): # 2 4 6 8
+#     print('hello', x)
+
+# for x in range(10,0,-1): # 2 4 6 8
+#     print('hello', x)
+
+# for x in range(10,-1,-1): # 2 4 6 8
+#     print('hello', x)
+
+
+# course = 'python'
+# for i in range(len(course)):
+#     print(course[i],i)
+
+# for i, c in enumerate(course):
+#     print(c, i)
