e3f9f81229b286d46c6c690b4330cb3a400285de
msa <msai.sadeghi@gmail.com>
2023-10-07 17:56:52 +0330

    s

--- session4.py
+++ session4.py
@@ -1,4 +1,4 @@
-# for i in range(5):
+# for i in range(5): # 01234
 #     print("hello", i)
 
 # for x in range(1,10):
@@ -7,23 +7,26 @@
 # for x in range(1,10,2): # 1 3 5 7 9
 #     print('hello', x)
 
-
 # for x in range(2,10,2): # 2 4 6 8
 #     print('hello', x)
 
-# for x in range(10,0,-1): # 2 4 6 8
+# for x in range(10,0,-1): 
 #     print('hello', x)
 
-# for x in range(10,-1,-1): # 2 4 6 8
+# for x in range(10,-1,-1): 
 #     print('hello', x)
 
 
-# course = 'python'
-# for i in range(len(course)):
-#     print(course[i],i)
+course = 'python'
+for c in course:
+    print(c.upper(), end=" ")
 
-# for i, c in enumerate(course):
-#     print(c, i)
+
+for i in range(len(course)):
+    print(course[i],i)
+
+for i, c in enumerate(course):
+    print(c, i)
 
 
 # با کمک حلقه فور برنامه ای بنویسید که
---
+++ session7.py
@@ -0,0 +1,70 @@
+# message = "hello my friends. welcome to python class"
+# for x in message:
+#     print(x)
+
+
+# for i in range(len(message)):
+#     print(message[i])
+
+# i = 0
+# while i < len(message):
+#     print(message[i])
+#     i += 1
+
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# for x in numbers:
+#     print(x)
+
+# for x in range(len(numbers)):
+#     print(numbers[x])
+
+# DO IT WITH WHILE LOOP
+
+
+# names = ["ayrana", "ailin", "iliya", "abtin"]
+# print(names)
+# print(len(names))
+
+# for i in range(3):
+#     new_student = input("enter a name: ")
+#     names.append(new_student)
+# print(names)
+# print(len(names))
+
+# failed_student = input("enter failed student's name: ")
+# names.remove(failed_student)
+# print(names)
+# print(len(names))
+
+# print("we want to remove before last student")
+# del names[-1]
+# print(names)
+# print(len(names))
+
+# print("we want to remove before last student")
+# names.pop()
+# print(names)
+# print(len(names))
+
+# TODO  تمرین
+# message = "we want to learn:python,pygame,javascript,java,c#,c++"
+
+"""
+تمرین اول
+کاراکترهای متن بالا را به صورت یکی در میان پرینت کنید 
+یک بار از حلقه فور استفاده کنید و  یک بار از حلقه وایل
+
+"""
+"""
+تمرین دوم
+از متن بالا نام زبان های برنامه نویسی را استحراج کنید و تعداد زبان های برنامه نویسی موجود در متن بالا را هم نمایش دهید.
+
+با کمک حلقه و 
+if
+:
+را پیدا کنید و از 
+: 
+به بعد برش بزنید تا اخز
+
+"""
