295a86480deaf2f9cece31f2ad3da9f341f2ffcd
msa <msai.sadeghi@gmail.com>
2023-10-28 18:00:56 +0330

    s

---
+++ question1.py
@@ -0,0 +1,33 @@
+# number = input("enter a number: ")
+# print("actual number:", number)
+# print("actual number:", number[0])
+
+# x = str(1245789543333333)
+# print(x[0])
+# print(x[1])
+# print(x[2])
+# print(x[3])
+
+# for i in x:
+#     print(i)
+
+# print(123 % 10)
+# print(123 // 10)
+# print(12 % 10)
+# print(12 // 10)
+# print(1 % 10)
+# print(1 // 10)
+
+
+# number = int(input("enter a number: "))
+
+# while number != 0:
+#     print(number % 10)
+#     number = number // 10
+
+# import random
+
+# students = ["iliya", "ayrana","abtin"]
+# print(random.choice(students))
+
+# print(random.randint(1000, 9999))
--- snake_game.py
+++ snake_game.py
@@ -1,11 +1,33 @@
 import turtle
 main_screen = turtle.Screen()
+main_screen.bgcolor('black')
+main_screen.setup(600, 600)
+main_screen.title("Snake Game")
+main_screen.tracer(False)
+
+
+def create_turtle(t_shape, t_color):
+    my_turtle = turtle.Turtle()
+    my_turtle.shape(t_shape)
+    my_turtle.color(t_color)
+    my_turtle.penup()
+    return my_turtle
+
+
+snake_head = create_turtle("square", "green")
+snake_food = create_turtle("circle", "red")
+snake_food.goto(100, 100)
+
+# TODO به جای اینکه در شروع بازی غذای مار در مکان ثابت قرار داشته باشد، مکان غذای مار تصادفی باشد
+
 
 def close():
-    pass
+    global running
+    running = False
+
 
 root = main_screen._root
-root.protocol("WM_WINDOW_DELETE", close)
+root.protocol("WM_DELETE_WINDOW", close)
 
 running = True
 while running:
